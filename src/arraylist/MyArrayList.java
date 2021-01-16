554ff0eb02face4ceea1a1602d5ac52a7c75ac7e
yanghao <1099761591@qq.com>
2021-01-16 16:23:49 +0800

    add code

--- src/arraylist/MyArrayList.java
+++ src/arraylist/MyArrayList.java
@@ -4,5 +4,19 @@ package arraylist;
  * @Author hao.yang
  * @Date 2021/1/16
  **/
-public class MyArrayList {
+public class MyArrayList<E> {
+    /**
+     * 容量
+     */
+    private int size;
+    /**
+     * 元素
+     */
+    private  E[] elements;
+    /**
+     * 默认容量
+     */
+    private static final int DEFAUT_CAPACITY = 10;
+
 }
+
