1104749e40ee7e5fbb5d009b301369fc842030b6
yanghao <10101010yh>
2021-06-08 11:28:49 +0800

    优化算法

--- src/alitest/PrintNumInTurnThread2.java
+++ src/alitest/PrintNumInTurnThread2.java
@@ -9,14 +9,13 @@ import java.util.concurrent.locks.LockSupport;
 public class PrintNumInTurnThread2 {
     private static Thread odd = null;
     private static Thread even = null;
-    private static volatile boolean oddHasRunFlag = false;
     public static void main(String[] args) {
 
        odd = new Thread(()->{
-          oddHasRunFlag = true;
            for (int i = 1; i < 1000; i+=2) {
                System.out.println(Thread.currentThread()+":"+i);
                LockSupport.unpark(even);
+               //执行到最后一步时，就不要阻塞线程了，让程序能正常结束
                if(i != 999){
                    LockSupport.park();
                }
@@ -24,18 +23,23 @@ public class PrintNumInTurnThread2 {
        });
 
         even = new Thread(()->{
-            while (oddHasRunFlag){
                 for (int i = 2; i < 1001; i+=2) {
-                    System.out.println(Thread.currentThread()+":"+i);
-                    LockSupport.unpark(odd);
-                    if(i != 1000){
+                    if(i != 1001){
+                        //偶数线程先park，能够保证奇数线程一定先打印；同时因为unpark可以在park之前,意味着奇数线程可以先生成许可，偶数线程必须先park去消费这个许可再打印，如果打印之后再消费可能出现偶数线程连续执行。
                         LockSupport.park();
                     }
-                }
-                break;
+                    System.out.println(Thread.currentThread()+":"+i);
+                    LockSupport.unpark(odd);
+
             }
         });
-        odd.start();
         even.start();
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        odd.start();
+
     }
 }
