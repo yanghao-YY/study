958797953e6c2018fae352eaa4ff39bf4b7d326e
yanghao <10101010yh>
2021-06-08 16:16:34 +0800

    增加算法

---
+++ src/alitest/PrintNumInTurnThread4.java
@@ -0,0 +1,36 @@
+package alitest;
+
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * @author hao.yang
+ * @date 2021/6/7
+ */
+public class PrintNumInTurnThread4 {
+    private static ReentrantLock reentrantLock = new ReentrantLock();
+    private static Condition condition = reentrantLock.newCondition();
+    private static volatile int num = 1;
+
+    public static void main(String[] args) {
+        for (int threadNum = 0; threadNum < 2 ; threadNum++) {
+            new Thread(()->{
+                for (int i = 0; i < 500; i++) {
+                    try {
+                        reentrantLock.lock();
+                        condition.signal();
+                        System.out.println(Thread.currentThread().getName()+":"+num);
+                        num++;
+                        if (i != 499){
+                            condition.await();
+                        }
+                    }catch ( Exception e){
+                        e.printStackTrace();
+                    }finally {
+                        reentrantLock.unlock();
+                    }
+                }
+            }).start();
+        }
+    }
+}
